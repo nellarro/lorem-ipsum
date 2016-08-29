86b77a776f437c03e77757e07b280ed3a202d539
Jenell Pizarro <nellarro@gmail.com>
2016-08-28 22:54:25 -0400

    Characters in place

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './style.sass'
 import Navigation from './Navigation'
 import Logo from './Logo'
 import Ads from './Ads'
+import Characters from './Characters'
 
 class App extends React.Component {
 
@@ -12,6 +13,7 @@ class App extends React.Component {
         <Navigation />
         <Logo />
         <Ads />
+        <Characters />
       </div>
     )
   }
